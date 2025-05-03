10e0fe2e60820d13ddbd28f7ae0333533088feb3
Mitchell Wintrow <90mitchwintrow@gmail.com>
2025-05-03 02:09:29 -0700

    feat: Add firebase

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,22 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import AppRouter from "./AppRouter";
 import { ThemeProvider } from "@/components/theme-provider";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FB_API_KEY,
+  authDomain: import.meta.env.VITE_FB_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FB_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FB_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FB_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FB_APP_ID,
+  measurementId: import.meta.env.VITE_FB_MEASUREMENT_ID,
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
 
 createRoot(document.getElementById("root")!).render(
   <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
